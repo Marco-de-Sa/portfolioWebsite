4a95eb65778834b8163f579f1410708dee542d0e
Marco-de-Sa <128144743+Marco-de-Sa@users.noreply.github.com>
2023-03-17 13:20:28 +0200

    Add files via upload

---
+++ ch11/chapter11.js
@@ -0,0 +1,28 @@
+var people = ["Kareem", "Fatma", "Sumaya", "Mr. Hobson", "Ms. Young", "Mrs. O' Doherty"];
+var otherPeople = ["Emilie", "Mommy", "Dad", "Kathy"];
+var differentPeople = ["Teddy", "Catchy", "Bobby"];
+
+people = people.concat(otherPeople);
+people = people.concat(differentPeople);
+
+//people = people.indexOf("Mr. Hobson");
+
+//people = people.join(" # ");
+
+//people = people.lastIndexOf("Bobby");
+
+document.getElementById("peopleIKnow").innerHTML = people.toString();
+
+people = p
+
+/*var people = ["Teddy", "Catchy", "Bobby"];
+console.log(people);
+people[0] = "Georgie";
+console.log(people);
+people[0] = "Mary";
+people[1] = "Bobby";
+people[2] = "Judy";
+people[3] = "Eddie";
+people[4] = "Herbie";
+people[5] = "Tony";
+console.log(people);*/
